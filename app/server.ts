7ae14a853812ed23dc70b6979a0848239f7c71dc
omar98alaa <omar98alaa@gmail.com>
2024-12-26 21:41:03 +0200

    initial commit

--- app/server.ts
+++ app/server.ts
@@ -1,13 +1,11 @@
 console.log("server entry point");
 
 import dotenv from "dotenv";
-import http from "http";
 import app from "./src/app.js";
 
 dotenv.config();
-const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
