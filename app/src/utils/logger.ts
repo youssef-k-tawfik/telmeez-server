14784c2996355b0fd3f252673b82be0a27af1071
Youssef Tawfik <youssef.k.tawfik@gmail.com>
2024-12-30 12:12:15 +0200

    added colors to the custom logger

--- app/src/utils/logger.ts
+++ app/src/utils/logger.ts
@@ -1,50 +1,65 @@
-export enum LogLevels{
-    ERROR,
-    WARN,
-    INFO,
-    DEBUG
+export enum LogLevels {
+  ERROR,
+  WARN,
+  INFO,
+  DEBUG,
+}
+
+enum LogColors {
+  RED     = 1,
+  GREEN   = 2,
+  YELLOW  = 3,
+  BLUE    = 4,
+  MAGENTA = 5,
+  CYAN    = 6,
+  WHITE   = 7,
 }
 
 export interface LogMsg {
-    level: LogLevels,
-    message: string,
+  level: LogLevels;
+  message: string;
 }
 
 export class Logger {
-    private static logLevel: LogLevels = LogLevels.INFO;
+  private static logLevel: LogLevels = LogLevels.INFO;
 
-    public static setLogLevel(level: LogLevels){
-        this.logLevel = level;
+  public static setLogLevel(level: LogLevels) {
+    this.logLevel = level;
+  }
+
+  public static log(logLevel: LogLevels, message: string) {
+    if (logLevel > this.logLevel) {
+      return;
     }
 
-    public static log(logLevel: LogLevels, message: string){
-        if(logLevel > this.logLevel){
-            return;
-        }
+    let level: string;
+    let color: LogColors;
 
-        let level: string;
+    switch (logLevel) {
+      case LogLevels.DEBUG:
+        level = "DEBUG";
+        color = LogColors.BLUE;
+        break;
 
-        switch (logLevel){
-            case LogLevels.DEBUG:
-                level = "DEBUG";
-                break;
+      case LogLevels.INFO:
+        level = "INFO";
+        color = LogColors.GREEN;
+        break;
 
-            case LogLevels.INFO:
-                level = "INFO";
-                break;
+      case LogLevels.WARN:
+        level = "WARN";
+        color = LogColors.YELLOW;
+        break;
 
-            case LogLevels.WARN:
-                level = "WARN";
-                break;
-
-            case LogLevels.ERROR:
-                level = "ERROR";
-                break;
-        }
-
-        const timestamp = new Date().toLocaleString();
-        const formattedMsg = `${timestamp} - [${level}]: ${message}`;
-
-        console.log(formattedMsg);
+      case LogLevels.ERROR:
+        level = "ERROR";
+        color = LogColors.RED;
+        break;
     }
+
+    const timestamp = new Date().toLocaleString();
+    const formattedMsg = `${timestamp} - [${level}]: ${message}`;
+
+    console.log(`\x1b[3${color}m%s\x1b[0m`, formattedMsg);
+  }
 }
